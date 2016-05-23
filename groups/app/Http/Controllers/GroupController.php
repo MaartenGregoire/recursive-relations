8babe6fef619c30fdb838c27e8148e504f30ee53
Maarten Gregoire <gregoiremaarten@gmail.com>
2016-05-23 20:53:20 +0200

    no eager loading

--- groups/app/Http/Controllers/GroupController.php
+++ groups/app/Http/Controllers/GroupController.php
@@ -9,13 +9,13 @@ class GroupController extends Controller
 {
     public function index()
     {
-        $data['groups'] = Group::with('allChildren')->where('parent_id', null)->get();
+        $data['groups'] = Group::where('parent_id', null)->get();
         return view('groups.index', $data);
     }
 
     public function show($id)
     {
-        $group = Group::with('allChildren')->where('id', $id)->first();
+       $group = Group::where('id', $id)->first();
         $data['group'] = $group;
         $data['children'] = join($this->createChildrenArray($group), ', ');
         return view('groups.show', $data);
@@ -24,7 +24,7 @@ class GroupController extends Controller
     private function createChildrenArray($group)
     {
         $childrenArray = [];
-        foreach($group->allChildren()->get() as $group){
+        foreach($group->children as $group){
             $childrenArray[] = $group->name;
             $childrenArray = array_merge($childrenArray, $this->createChildrenArray($group));
         }
